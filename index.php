6f65882c9b16c070d909b0b460ee6b4b48fdff78
ArtemkAlp <alpatov.tema@gmail.com>
2019-11-05 09:55:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h2>Здание 1</h2>
+		<?php
+			for($i=1900;$i<=2100;$i++){
+				echo $i;
+				}
+		?>
+		<h2>Задание 2</h2>
+		<form method="POST">
+			Введите значение коэффициента A:<input type="action" name="A"><br/>
+			Введите значение коэффициента B:<input type="action" name="B"><br/>
+			Введите значение коэффициента C:<input type="action" name="C"><br/>
+			<input type="submit" value="OK"
+		</form>
+		<?php
+			$A="$_POST(A)";
+			$B="$_POST(B)";
+			$C="$_POST(C)";
+			
+			$D=$B*$B-4*$A*$C;
+			
+			if($D<0) {
+				echo "Корней нет";
+				}
+			else {
+				$X1=(-$B-sqrt($D))/(2*$A);
+				$X2=(-$B+sqrt($D))/(2*$A);
+				echo $X1;
+				echo $X2;
+				}
+		?>
+	</body>
+</html>
